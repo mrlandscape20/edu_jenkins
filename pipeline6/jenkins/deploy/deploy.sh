92ec8b6d633319f2a136c753c270e9334617dd58
mrlandscape20 <66933774+mrlandscape20@users.noreply.github.com>
2021-05-17 21:56:23 +0700

    Update deploy.sh

--- pipeline6/jenkins/deploy/deploy.sh
+++ pipeline6/jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -o StrictHostKeyChecking=no /tmp/.auth root@113.164.246.30:/tmp/.auth
-scp -o StrictHostKeyChecking=no ./pipeline6/jenkins/deploy/publish root@113.164.246.30:/tmp/publish
-ssh -o StrictHostKeyChecking=no root@113.164.246.30 "bash /tmp/publish"
+scp /tmp/.auth root@10.127.17.130:/tmp/.auth
+scp ./pipeline6/jenkins/deploy/publish root@10.127.17.130:/tmp/publish
+ssh root@10.127.17.130 "bash /tmp/publish"
