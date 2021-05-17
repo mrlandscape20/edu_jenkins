70f94ba13bbb63e594ad83b4d816d63edd7be4a4
mrlandscape20 <66933774+mrlandscape20@users.noreply.github.com>
2021-05-17 21:36:11 +0700

    Update deploy.sh

--- pipeline6/jenkins/deploy/deploy.sh
+++ pipeline6/jenkins/deploy/deploy.sh
@@ -5,5 +5,5 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 scp -o StrictHostKeyChecking=no /tmp/.auth root@113.164.246.20:/tmp/.auth
-scp -o StrictHostKeyChecking=no./pipeline6/jenkins/deploy/publish root@113.164.246.20:/tmp/publish
+scp -o StrictHostKeyChecking=no ./pipeline6/jenkins/deploy/publish root@113.164.246.20:/tmp/publish
 ssh -o StrictHostKeyChecking=no root@113.164.246.20 "bash /tmp/publish"
